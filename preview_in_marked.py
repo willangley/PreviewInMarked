eb8875307b21d6c2fef5b4030f2ab600d1bfec3a
Will Angley <will@willangley.org>
2021-02-19 20:14:33 -0500

    Only send the file URL to Marked on first call
    It will remember the URL on subsequent stream updates, and this improves performance meaningfully.

--- preview_in_marked.py
+++ preview_in_marked.py
@@ -26,8 +26,9 @@ from rubicon.objc import objc_const
 from rubicon.objc.runtime import load_library
 
 CoreServices = load_library('CoreServices')
-NSURL = ObjCClass("NSURL")
+NSURL = ObjCClass('NSURL')
 NSPasteboard = ObjCClass('NSPasteboard')
+NSWorkspace = ObjCClass('NSWorkspace')
 kUTTypeUTF8PlainText = objc_const(CoreServices, 'kUTTypeUTF8PlainText')
 
 
@@ -35,10 +36,15 @@ class PreviewInMarked(sublime_plugin.ViewEventListener):
     def __init__(self, view):
         super(PreviewInMarked, self).__init__(view)
         self.pb_ = NSPasteboard.pasteboardWithName('mkStreamingPreview')
+        self.setup_ = True
 
     def on_init(self):
         self.view.settings().add_on_change('preview_in_marked',
                                            self.on_modified)
+
+    def on_settngs_change(self):
+        self.setup_ = True
+        self.on_modified()
 
     def on_modified(self):
         if not self.view.settings().get('preview_in_marked', False):
@@ -47,13 +53,16 @@ class PreviewInMarked(sublime_plugin.ViewEventListener):
         raw_string = self.view.substr(sublime.Region(0, self.view.size()))
         self.pb_.clearContents()
 
-        # TODO(willangley): debug this; Marked 2 beachballs with URLs.
+        # Marked 2 hangs for a minute on my machine after receiving a file URL,
+        # so only send one when switching which file is being streamed.
         # http://support.markedapp.com/discussions/problems/161844-streaming-preview-beachballs-when-url-is-supplied
-        if self.view.file_name():
+        if self.view.file_name() and self.setup_:
             base_url = NSURL.fileURLWithPath(self.view.file_name())
             self.pb_.writeObjects([raw_string, base_url])
         else:
             self.pb_.setString(raw_string, forType=kUTTypeUTF8PlainText)
+
+        self.setup_ = False
 
 
 class PreviewInMarkedCommand(sublime_plugin.TextCommand):
